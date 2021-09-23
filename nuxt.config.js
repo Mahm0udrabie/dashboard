4668f2171f1e7cc74efa807b19036fa47aaa131e
Mahmoud Rabie <Mahmoudahmed9571@outlook.com>
2021-09-23 03:09:09 +0200

    fix axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,8 +83,8 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // baseURL: 'https://shrouded-spire-95079.herokuapp.com/api'
-    baseURL: 'http://127.0.0.1:8000/api'
+    baseURL: 'https://shrouded-spire-95079.herokuapp.com/api'
+    // baseURL: 'http://127.0.0.1:8000/api'
   },
 
 
